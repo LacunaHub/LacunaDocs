7a3fdee218d6f4d11cad1c1de10a246cacc69f85
rovius <rovius@users.noreply.github.com>
2024-06-08 18:17:22 +0300

    feat(components): `MdnRef` component

---
+++ src/components/MdnRef.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MdnRef({ dataType }) {
+    return (
+        <a href={`https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/${dataType}`}>{dataType.toLowerCase()}</a>
+    );
+}
