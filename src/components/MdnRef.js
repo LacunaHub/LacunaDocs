d4812f42560dceebfeebeb8ef02cf75e117a5cfb
rovius <rovius@users.noreply.github.com>
2024-06-08 18:32:40 +0300

    chore(MdnRef): remove unused react import

--- src/components/MdnRef.js
+++ src/components/MdnRef.js
@@ -1,7 +1,5 @@
-import React from 'react';
-
 export default function MdnRef({ dataType }) {
     return (
-        <a href={`https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/${dataType}`}>{dataType.toLowerCase()}</a>
+        <a href={`https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/${dataType}`}>{dataType}</a>
     );
 }
