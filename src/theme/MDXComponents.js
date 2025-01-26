59cf2297df54b9c1bfd012d7d669d5352760891c
Daniyar Kurmangaliyev <randomna79@gmail.com>
2025-01-26 08:29:28 +0500

    feat: update documentation for custom behavior data types and add new entries

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,10 +2,14 @@
 import DocRef from '@site/src/components/DocRef'
 import MdnRef from '@site/src/components/MdnRef'
 import MDXComponents from '@theme-original/MDXComponents'
+import TabItem from '@theme/TabItem'
+import Tabs from '@theme/Tabs'
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   MdnRef,
-  DocRef
+  DocRef,
+  Tabs,
+  TabItem
 }
